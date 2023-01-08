db1a965a3fc2a140eea68fa315b5f2185d71a7d5
tpAtalas <tp@atalas.dev>
2023-01-08 08:23:14 -0600

    feat: Added the number counts of todo to `FooterSidebarMenu` (#128)
    Added the `totalNumberTodos` based on the pathname. Each count will be
    re-rendering as `useRecoilValue` is used instead of `useRecoilCallback`

---
+++ components/layouts/layoutApp/layout/layoutFooter/footerSidebar/todosCount.tsx
@@ -0,0 +1,11 @@
+import { PATHNAME } from '@data/stateObjects';
+import { selectorFilterTodoIdsByPathname } from '@states/todoStates';
+import { Fragment } from 'react';
+import { useRecoilValue } from 'recoil';
+
+export const TodosCount = ({ pathname }: { pathname: PATHNAME }) => {
+  const todoIdsByPathName = useRecoilValue(selectorFilterTodoIdsByPathname(pathname));
+  const todoCounts = todoIdsByPathName.length;
+
+  return <Fragment>{todoCounts}</Fragment>;
+};
