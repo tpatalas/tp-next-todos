ee23960f6247784293e8e1ee4e1eaa1ad856af90
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-01-30 10:08:49 -0600

    test: Added a new testing for routing
    Added and performed the testing for routing to home upon clicking the Logo.

--- components/layouts/layoutApp/__test__/layoutLogo.test.tsx
+++ components/layouts/layoutApp/__test__/layoutLogo.test.tsx
@@ -1,12 +1,21 @@
 import { renderWithRecoilRoot } from '@states/utils';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
+import mockRouter from 'next-router-mock';
 import { LayoutLogo } from '../layoutLogo';
 
 describe('LayoutLogo', () => {
   renderWithRecoilRoot(<LayoutLogo />);
 
-  it('should render the logo', () => {
-    const logoComponent = screen.getByText(/I am a LOGO/i);
+  let logoComponent: HTMLElement;
+
+  it('should render the logo', async () => {
+    logoComponent = await screen.findByText(/I am a LOGO/i);
     expect(logoComponent).toBeInTheDocument();
+  });
+
+  it('should route to the base URL', async () => {
+    mockRouter.push('/');
+    fireEvent.click(logoComponent);
+    expect(mockRouter).toMatchObject({ asPath: '/', pathname: '/' });
   });
 });
