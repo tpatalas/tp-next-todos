44fe19589c2292fc6d1ecac38f91feb13080b69a
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-01-31 15:02:55 -0600

    feat: Increase the media size of lg by 100px

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       // => @media (min-width: 640px) { ... }
       sm: '640px',
       md: '768px',
-      lg: '1024px',
+      lg: '1124px',
       xl: '1280px',
       '2xl': '1536px',
       '3xl': '1792px',
