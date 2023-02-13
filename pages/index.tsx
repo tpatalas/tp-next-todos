d529e849af54b7d2d573a3ec2f7e55e44ea66801
tpAtalas <tp@atalas.dev>
2023-02-13 15:17:03 -0600

    feat: add new middleware to rewrite the directory (#245)
    add rewrite the home directory ('/') to the app directory ('/app') in
    the middleware. The home directory, intended for the static page,
    is current not in use in this application.
    
    Core changes:
    - feat: add rewrite the home directory to app directory.
    - refactor: remove unnecessary contents from the middleware.
    - refactor: remove unnecessary contents from the home directory.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,10 @@
-import { PATHNAME } from '@data/dataTypesObjects';
-import { STYLE_BUTTON_NORMAL_BLUE } from '@data/stylePreset';
 import { LayoutHome } from '@layouts/layoutHome';
-import dynamic from 'next/dynamic';
 import { ReactElement } from 'react';
-
-const PrefetchRouterButton = dynamic(
-  () => import('@buttons/button/prefetchRouterButton').then((mod) => mod.PrefetchRouterButton),
-  { ssr: false },
-);
 
 const Home = () => {
   return (
-    <div className='flex w-full flex-row justify-center'>
-      <PrefetchRouterButton
-        className={STYLE_BUTTON_NORMAL_BLUE}
-        path={PATHNAME['app']}>
-        Go to App
-      </PrefetchRouterButton>
+    <div className='mt-10 flex w-full flex-row items-center justify-center text-2xl'>
+      This area is intentionally left blank for now.
     </div>
   );
 };
