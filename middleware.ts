4d8a5b8b43638e940ae840605bcf047791f7d173
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-02-13 13:34:24 -0600

    feat: add new middleware to rewrite the directory
    add rewrite the home directory ('/') to the app directory ('/app') in
    the middleware. The home directory, intended for the static page,
    is current not in use in this application.
    
    Core changes:
    - feat: add rewrite the home directory to app directory.
    - refactor: remove unnecessary contents from the middleware.
    - refactor: remove unnecessary contents from the home directory.

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,11 @@
-import { NextResponse } from 'next/server';
-//TODO: Update the Middleware for the protection setup such as api route.
-// Example: https://blog.tericcabrel.com/protect-your-api-routes-in-next-js-with-middleware/
+import { NextRequest, NextResponse } from 'next/server';
 
-// const isTodosRoute = (pathname: string) => {
-//   return pathname.startsWith('/api/todos');
-// };
-
-export async function middleware() {
-  // const { pathname } = req.nextUrl;
-
-  // if (isTodosRoute(pathname)) {
-  //   return NextResponse.redirect(new URL('/403', req.url));
-  // }
-
-  return NextResponse.next();
+export async function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.match('/')) {
+    return NextResponse.rewrite(new URL('/app', req.url));
+  }
 }
 
 export const config = {
-  matcher: ['/api/todos/:path*'],
+  matcher: ['/'],
 };
