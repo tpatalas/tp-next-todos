40014632681aa9957ee42d36548f797d733afaef
tpAtalas <tp@atalas.dev>
2023-02-24 14:03:28 -0600

    fix: fix the issues of keybinding and styles (#285)
    This branch fixes a minor keybinding issue related to the itemLabelModal,
    as well as a styling issue with the footerBody on different media queries.
    
    Core Changes:
    - fix: fix the Key does not work on editing with LabelModal (#283)
    - fix: resolve the footerBody's height rendering issue on different media
    queries (#284)

--- components/layouts/layoutApp/layout/layoutHeader/index.tsx
+++ components/layouts/layoutApp/layout/layoutHeader/index.tsx
@@ -26,7 +26,7 @@ export const LayoutHeader = () => {
 
   return (
     <LayoutHeaderFragment>
-      <div className='sticky top-1 z-10 mb-2 flex max-h-[4rem] min-h-[4rem] flex-row items-center justify-between bg-transparent'>
+      <div className='sticky top-1 z-10 flex max-h-[4rem] min-h-[4rem] flex-row items-center justify-between bg-transparent sm:mb-2'>
         <LeftSideFragment>
           <div className='flex flex-row items-center justify-between pl-3 md:w-full md:max-w-3xs'>
             <SidebarButtonFragment>
