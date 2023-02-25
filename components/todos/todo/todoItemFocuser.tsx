858bf2cc8be2b0c3b897d50d53db463b5a768ede
tpAtalas <tp@atalas.dev>
2023-02-24 18:59:30 -0600

    feat: improve UXs on different media queries and devices
    improve the keybindings and styles on different media queries and devices,
    such as mobile and desktop browsers.
    
    Core Changes:
    feat: disable keybindings on Mobiles (#288).
    feat: remove unnecessary styles and fix mismatches (#289).
    style: improve styles on different media queries and devices (#290).

--- components/todos/todo/todoItemFocuser.tsx
+++ components/todos/todo/todoItemFocuser.tsx
@@ -4,6 +4,7 @@ import { KeysWithNavigationEffect } from '@states/keybinds/KeysWithNavigateEffec
 import { classNames } from '@states/utils';
 import { Types, TypesTodo } from 'lib/types';
 import { Fragment as FocuserFragment, useRef } from 'react';
+import { isMobile } from 'react-device-detect';
 
 type Props = Pick<TypesTodo, 'todo' | 'index'> & Pick<Types, 'children'>;
 
@@ -18,7 +19,8 @@ export const TodoItemFocuser = ({ todo, index, children }: Props) => {
         tabIndex={0}
         className={classNames(
           'group/focuser mr-1 flex flex-row rounded-lg px-2 pt-4 pb-2 outline-none hover:bg-slate-100 focus:bg-blue-100 sm:px-5',
-          'w-[calc(100vw-5rem)] max-w-3xl sm:w-[calc(100vw-7rem)] md:w-[calc(65vw-5rem)] ml:w-[calc(70vw-5rem)]',
+          'w-[calc(100vw-5rem)] max-w-3xl md:w-[calc(65vw-5rem)] ml:w-[calc(70vw-5rem)]',
+          isMobile ? 'sm:w-[calc(100vw-13rem)]' : 'sm:w-[calc(100vw-7rem)]',
         )}
         ref={divFocus}
         onKeyDown={focusKeyHandler}
