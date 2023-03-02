4b461272ae3b28ce2a5c9223f1e000723631c73c
tpAtalas <tp@atalas.dev>
2023-03-01 20:06:11 -0600

    feat: add the basic setup for authentication (#313)
    Installed the new packages called nextAuth and bcryptjs. nextAuth will
    be used for the authentication process and bcryptjs will be used for
    encrypting passwords.
    
    Added new util functions to handle the authentication process:
    hashDataString, hasStrongPassword, and hasEmailFormat.
    hashDataString is equipped with bcryptjs to encrypt strings such as
    password. hasStrongPassword checks if a password is at least 8
    characters long, has one uppercase, one lowercase, one special character,
    and one number. hasEmailFormat simply validates whether a given
    email address is in a standard format.
    
    Updated the UsersSchema and User API, and added a sample
    authentication page with authForm.

---
+++ pages/auth.tsx
@@ -0,0 +1,18 @@
+import { AuthForm } from '@components/auth/authForm';
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+const Auth = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>My Todo App: Log in</title>
+      </Head>
+      <div className='mt-20 flex flex-row items-center justify-center'>
+        <AuthForm />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Auth;
