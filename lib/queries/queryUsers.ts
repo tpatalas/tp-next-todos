30433379c445216de5a5ca513ceb4101c9439959
tpAtalas <tp@atalas.dev>
2023-03-01 20:06:11 -0600

    feat: add the basic setup for authentication (#313)
    Installed the new packages called nextAuth and bcryptjs. nextAuth will
    be used for the authentication process and bcryptjs will be used for
    encrypting passwords.
    
    Added new util functions to handle the authentication process:
    hashDataString, hasStrongPassword, and hasEmailFormat.
    hashDataString is equipped with bcryptjs to encrypt strings such as
    password. hasStrongPassword checks if a password is at least 8
    characters long, has one uppercase, one lowercase, one special character,
    and one number. hasEmailFormat simply validates whether a given
    email address is in a standard format.
    
    Updated the UsersSchema and User API, and added a sample
    authentication page with authForm.

---
+++ lib/queries/queryUsers.ts
@@ -0,0 +1,14 @@
+import { Users } from '@lib/types';
+import { fetchWithRetry } from '@states/utils';
+
+const apiUsers = process.env.NEXT_PUBLIC_API_ENDPOINT_USERS as string;
+
+export const createDataNewUser = async (data: Users) => {
+  const response = await fetchWithRetry(apiUsers, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) throw new Error(response.statusText);
+  return await response.json();
+};
