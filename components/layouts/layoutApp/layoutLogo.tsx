5235682535f2184d1625a288e2d5976f1b6ef7f6
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-03-14 21:05:41 -0500

    feat: add temporary text logo placement

--- components/layouts/layoutApp/layoutLogo.tsx
+++ components/layouts/layoutApp/layoutLogo.tsx
@@ -12,8 +12,14 @@ export const LayoutLogo = () => {
             className: 'cursor-pointer',
           }}>
           <LogoFragment>
-            <span className='sr-only'>Logo</span>
-            <div>I am a LOGO</div>
+            <div className='ml-2 flex w-full flex-row items-center justify-center text-2xl tracking-wider'>
+              <span className='sr-only'>Logo</span>
+              <div className='-space-y-[0.4rem] pr-3 text-sm font-bold tracking-widest'>
+                <div>LO</div>
+                <div>GO</div>
+              </div>
+              <div>TODO</div>
+            </div>
           </LogoFragment>
         </PrefetchRouterButton>
       </LogoContainerFragment>
