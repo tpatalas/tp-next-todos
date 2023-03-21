7d0775ec1d5600f267ac7977659d837f3a2a4f39
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-03-21 09:49:02 -0500

    fix: prevent demo-session from randomly returning false session
    This commit addresses an issue where the demo-session would occasionally
    return a false session upon a hard refresh. The root cause was identified
    as the brief 'loading' state of the `status` from the `useSession()` hook
    provided by `next-auth`.
    
    To resolve this issue, the handling of each `status` step has been
    explicitly defined, ensuring proper behavior during the 'loading' state.
    As a result, the `queryEffect` no longer retrieves the session state from
    sessionStorage or localStorage. Instead, it now uses
    `getLoadable(atomUserOffSession)` for a more native Recoil-based approach.

--- lib/states/users/userSessionEffect.tsx
+++ lib/states/users/userSessionEffect.tsx
@@ -1,59 +1,32 @@
 import { STORAGE_KEY } from '@data/dataTypesConst';
-import { atomQueryLabels } from '@states/labels/atomQueries';
-import { atomQueryTodoIds } from '@states/todos/atomQueries';
-import { getSessionStorage, setSessionStorage } from '@states/utils';
-import { deleteDB } from 'idb';
+import { delSessionStorage, getSessionStorage, setSessionStorage } from '@states/utils';
 import { useSession } from 'next-auth/react';
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useRecoilCallback } from 'recoil';
-import { atomIDBUserSession } from '.';
+import { atomUserOffSession } from '.';
 
 export const UserSessionEffect = () => {
-  const { data: session } = useSession();
-  const notInSession = session === null && session !== undefined;
+  const { status } = useSession();
 
-  const clearIndexedDB = async () => {
-    const indexedDBs = await indexedDB.databases();
-
-    await Promise.all(indexedDBs.map((idb) => idb && deleteDB(idb.name as string)));
-  };
-
-  const userSession = useRecoilCallback(({ set, reset }) => () => {
-    if (session) {
-      set(atomIDBUserSession, true);
-      setSessionStorage(STORAGE_KEY['session'], true);
-    }
-    if (notInSession) {
-      setSessionStorage(STORAGE_KEY['session'], false);
-      set(atomIDBUserSession, false);
-      reset(atomQueryTodoIds);
-      reset(atomQueryLabels);
-      localStorage.clear();
-      clearIndexedDB();
+  const session = useRecoilCallback(({ set }) => () => {
+    if (status === 'unauthenticated') {
+      set(atomUserOffSession, true);
+      setSessionStorage(STORAGE_KEY['offSession'], true);
       return;
+    }
+    if (status === 'authenticated') {
+      set(atomUserOffSession, false);
+      delSessionStorage(STORAGE_KEY['offSession']);
+      return;
+    }
+    if (status === 'loading' && !getSessionStorage(STORAGE_KEY['offSession'])) {
+      set(atomUserOffSession, false);
     }
   });
 
-  const setSession = useCallback(() => {
-    const isSession = getSessionStorage(STORAGE_KEY['session']);
-
-    if (notInSession) {
-      !isSession && setSessionStorage(STORAGE_KEY['session'], false);
-      return;
-    }
-
-    if (session) return !isSession && setSessionStorage(STORAGE_KEY['session'], true);
-  }, [notInSession, session]);
-
   useEffect(() => {
-    userSession();
-    window.addEventListener('storage', setSession);
-    return () => {
-      window.removeEventListener('storage', setSession);
-    };
-    // removed the userSession from the dependency as It will clear out in offSession data.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [setSession]);
+    session();
+  }, [session, status]);
 
   return null;
 };
