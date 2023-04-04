cce53d5c4f1469493b05bd0a12c72e7ef8f301d7
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-04 15:04:52 -0500

    refactor: enhance signInButton reusability
    Improve the signInButton component by accepting an optional object props,
    making it more reusable. Add signInButtonName prop, including its types.
    
    Resolve minor conflicts resulting from merged branches.

--- lib/types/bases/ui.ts
+++ lib/types/bases/ui.ts
@@ -1,4 +1,11 @@
-import { DURATION, GRADIENT_TYPE, GRADIENT_POSITION, VIEWBOX, POSITION_X, POSITION_Y } from '@constAssertions/ui';
+import {
+  DURATION,
+  GRADIENT_TYPE,
+  GRADIENT_POSITION,
+  VIEWBOX,
+  POSITION_X,
+  POSITION_Y,
+} from '@constAssertions/ui';
 import {
   ReactNode,
   ReactElement,
@@ -55,6 +62,7 @@ export interface TypesUi {
   isPortal: boolean;
   gradientType: GRADIENT_TYPE;
   gradientPosition: GRADIENT_POSITION;
+  signInButtonName: 'Sign in' | 'Get started';
 }
 
 export interface TypesLoadings {
