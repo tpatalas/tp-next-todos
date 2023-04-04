923e550fc36d39503baad48e18473d7835de852e
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-04 14:29:21 -0500

    refactor: enhance signInButton reusability
    Improve the signInButton component by accepting an optional object props,
    making it more reusable. Add signInButtonName prop, including its types.
    
    Resolve minor conflicts resulting from merged branches.

--- components/layouts/layoutHeader/signInButton.tsx
+++ components/layouts/layoutHeader/signInButton.tsx
@@ -1,18 +1,25 @@
 import { Button } from '@buttons/button';
 import { STYLE_BUTTON_NORMAL_BLUE } from '@data/stylePreset';
+import { Types } from '@lib/types';
 import { classNames } from '@stateLogics/utils';
 import { signIn } from 'next-auth/react';
 
-export const SignInButton = () => {
+type Props = { options?: Partial<Pick<Types, 'signInButtonName'>> };
+
+const buttonOptions = {
+  className: classNames(STYLE_BUTTON_NORMAL_BLUE, 'max-ml:w-full max-ml:mb-3'),
+  tooltip: 'Sign in',
+};
+
+export const SignInButton = ({ options }: Props) => {
+  const { signInButtonName = 'Sign in' } = options ?? {};
+
   return (
     <>
       <Button
-        options={{
-          className: classNames(STYLE_BUTTON_NORMAL_BLUE, 'max-ml:w-full max-ml:mb-3'),
-          tooltip: 'Sign in',
-        }}
+        options={buttonOptions}
         onClick={() => signIn()}>
-        Sign in
+        {signInButtonName}
       </Button>
     </>
   );
