7aa89bb2d39c86efcbc2b371b2b62d3e36b880bb
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-05 14:02:41 -0500

    feat: migrate LayoutBodyTagEffect to groupEffect
    Transform the LayoutBodyTagEffect component into the useLayoutBodyTagClass
    custom hook, integrating it into the GroupEffect components of LayoutApp
    and LayoutHome.
    
    Relocate the GroupEffect components from the 'effects/' directory to a
    new 'groupEffects/' directory. Update module aliases in tsconfigs and
    jest.configs accordingly.
    
    Remove unnecessary components as part of the refactoring process.

--- lib/stateLogics/groupEffects/layout.tsx
+++ lib/stateLogics/groupEffects/layout.tsx
@@ -1,36 +1,28 @@
+import { Notification } from '@components/notifications/notification';
+import { CATCH } from '@constAssertions/misc';
 import {
   useFilterPathApp,
   useFilterPathHome,
   useInitialNavigation,
+  useLayoutBodyTagClass,
   useLayoutType,
 } from '@hooks/layouts';
-import { GroupEffects } from './groupEffects';
-import dynamic from 'next/dynamic';
-import { CATCH } from '@constAssertions/misc';
+import { LabelModal } from '@modals/labelModals/labelModal';
+import { MinimizedModal } from '@modals/minimizedModal';
+import { TodoModal } from '@modals/todoModals/todoModal';
 import { atomCatch } from '@states/misc';
 import { useRecoilValue } from 'recoil';
-
-const CreateTodoModal = dynamic(() =>
-  import('@modals/todoModals/todoModal').then((mod) => mod.TodoModal),
-);
-const MinimizedModal = dynamic(() =>
-  import('@modals/minimizedModal').then((mod) => mod.MinimizedModal),
-);
-const Notification = dynamic(() =>
-  import('components/notifications/notification').then((mod) => mod.Notification),
-);
-const LabelModal = dynamic(() =>
-  import('@modals/labelModals/labelModal').then((mod) => mod.LabelModal),
-);
+import { GroupEffects } from '.';
 
 export const LayoutHomeGroupEffects = () => {
   const filterPath = useFilterPathHome();
   const setNavigation = useInitialNavigation({ layoutType: 'home' });
   const setLayoutType = useLayoutType({ layoutType: 'home' });
+  const setBodyTagClass = useLayoutBodyTagClass({ layoutType: 'home' });
 
   return (
     <>
-      <GroupEffects effects={[filterPath, setLayoutType, setNavigation]} />
+      <GroupEffects effects={[filterPath, setLayoutType, setNavigation, setBodyTagClass]} />
     </>
   );
 };
@@ -40,12 +32,13 @@ export const LayoutAppGroupEffects = () => {
   const setNavigation = useInitialNavigation({ layoutType: 'app' });
   const setLayoutType = useLayoutType({ layoutType: 'app' });
   const catchTodoModal = useRecoilValue(atomCatch(CATCH.todoModal));
+  const setBodyTagClass = useLayoutBodyTagClass({ layoutType: 'app' });
 
   return (
     <>
-      <GroupEffects effects={[filterPath, setLayoutType, setNavigation]} />
+      <GroupEffects effects={[filterPath, setLayoutType, setNavigation, setBodyTagClass]} />
       <Notification />
-      <CreateTodoModal />
+      <TodoModal />
       <MinimizedModal />
       {!catchTodoModal && <LabelModal />}
     </>
