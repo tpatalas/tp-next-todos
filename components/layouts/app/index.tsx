86bb829c8b48cfa3654b99695a67bd925068fadd
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-05 11:01:59 -0500

    feat: implement the group effects
    Refactor the LayoutApp and LayoutHome components to use custom hooks
    for effects. Each custom hook is now passed as a prop to the GroupEffect
      component, which dynamically handles the side effects of each hook.
    
    To enhance performance, group effects are wrapped in specialized components
    and imported dynamically. This approach optimizes bundle size and
    improves the overall efficiency of the application.

--- components/layouts/app/index.tsx
+++ components/layouts/app/index.tsx
@@ -1,33 +1,9 @@
-import { CATCH } from '@constAssertions/misc';
-import { LayoutTypeEffect } from '@effects/layoutTypeEffect';
-import { NavigationInitialEffect } from '@effects/navigationInitialEffect';
 import { Types } from '@lib/types';
-import { atomCatch, atomHtmlTitleTag } from '@states/misc';
+import { atomHtmlTitleTag } from '@states/misc';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import {
-  Fragment as EffectFragment,
-  Fragment as HeaderFragment,
-  Fragment as LayoutAppFragment,
-  Fragment as ModalActionsFragment,
-  Suspense,
-} from 'react';
+import { Fragment as HeaderFragment, Fragment as LayoutAppFragment, Suspense } from 'react';
 import { useRecoilValue } from 'recoil';
-const CreateTodoModal = dynamic(() =>
-  import('@modals/todoModals/todoModal').then((mod) => mod.TodoModal),
-);
-const MinimizedModal = dynamic(() =>
-  import('@modals/minimizedModal').then((mod) => mod.MinimizedModal),
-);
-const Notification = dynamic(() =>
-  import('components/notifications/notification').then((mod) => mod.Notification),
-);
-const LabelModal = dynamic(() =>
-  import('@modals/labelModals/labelModal').then((mod) => mod.LabelModal),
-);
-const WindowBeforeunloadEffect = dynamic(() =>
-  import('@effects/windowBeforeunloadEffect').then((mod) => mod.WindowBeforeunloadEffect),
-);
 const FooterBody = dynamic(() =>
   import('@layouts/layoutFooter/footerBody').then((mod) => mod.FooterBody),
 );
@@ -39,18 +15,19 @@ const LayoutFooter = dynamic(
   () => import('@layouts/layoutFooter').then((mod) => mod.LayoutFooter),
   { ssr: false },
 );
+
+const LayoutAppGroupEffects = dynamic(
+  () => import('@effects/layout').then((mod) => mod.LayoutAppGroupEffects),
+  { ssr: false },
+);
+
 const User = dynamic(() => import('@layouts/layoutHeader/user').then((mod) => mod.User), {
   ssr: false,
 });
-const FilterPathAppEffect = dynamic(
-  () => import('@effects/filters/filterPathAppEffect').then((mod) => mod.FilterPathAppEffect),
-  { ssr: false },
-);
 
 type Props = Pick<Types, 'children'>;
 
 export const LayoutApp = ({ children }: Props) => {
-  const catchTodoModal = useRecoilValue(atomCatch(CATCH.todoModal));
   const slug = useRecoilValue(atomHtmlTitleTag);
 
   return (
@@ -71,18 +48,7 @@ export const LayoutApp = ({ children }: Props) => {
           </LayoutFooter>
         </div>
       </HeaderFragment>
-      <EffectFragment>
-        <FilterPathAppEffect />
-        <LayoutTypeEffect layoutType='app' />
-        <NavigationInitialEffect layoutType='app' />
-        <Notification />
-        <WindowBeforeunloadEffect />
-        <ModalActionsFragment>
-          <CreateTodoModal />
-          <MinimizedModal />
-          {!catchTodoModal && <LabelModal />}
-        </ModalActionsFragment>
-      </EffectFragment>
+      <LayoutAppGroupEffects />
     </LayoutAppFragment>
   );
 };
