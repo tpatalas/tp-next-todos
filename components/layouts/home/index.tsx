1b5736eae949012dc1b791382a83e8e7d2a2ad4c
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-09 11:39:27 -0500

    feat: integrate Footer component into LayoutHome
    Incorporate the Footer section component into LayoutHome, ensuring it
    is rendered on every page within the home layout. The Footer section
    component has not been added to LayoutApp, as it is not required within that
    layout.

--- components/layouts/home/index.tsx
+++ components/layouts/home/index.tsx
@@ -5,10 +5,9 @@ import { Fragment as LayoutFragment, ReactNode } from 'react';
 import { useRecoilValue } from 'recoil';
 
 const HomeNavigation = dynamic(() => import('./homeNavigation').then((mod) => mod.HomeNavigation));
-
 const LayoutHeader = dynamic(() => import('@layouts/layoutHeader').then((mod) => mod.LayoutHeader));
-
 const LayoutFooter = dynamic(() => import('@layouts/layoutFooter').then((mod) => mod.LayoutFooter));
+const Footer = dynamic(() => import('@components/sections/footer').then((mod) => mod.Footer));
 
 const LayoutHomeGroupEffects = dynamic(
   () => import('@groupEffects/layout').then((mod) => mod.LayoutHomeGroupEffects),
@@ -32,6 +31,7 @@ export const LayoutHome = ({ children }: Props) => {
       </LayoutHeader>
       <LayoutFooter layoutType='home' />
       {children}
+      <Footer />
       <LayoutHomeGroupEffects />
     </LayoutFragment>
   );
