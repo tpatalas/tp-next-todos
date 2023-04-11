920ebb969fb436300a11e5f2fd3ac2a3331c2456
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-10 22:51:52 -0500

    feat: optimize Header section component rendering
    Improve the rendering of the Header section component across different
    media queries by utilizing a new custom hook. This hook returns the
    current client screen width, which can be used within the Header
    section component to optimize its layout and responsiveness.

--- components/sections/header.tsx
+++ components/sections/header.tsx
@@ -1,12 +1,15 @@
 import { STYLE_BLUR_GRADIENT_B_MD } from '@data/stylePreset';
-import { useVerticalScrollPosition } from '@hooks/misc';
+import { useVerticalScrollPosition, useWindowWidth } from '@hooks/misc';
 import { classNames } from '@stateLogics/utils';
 
 export const Header = () => {
   const scrollPosition = useVerticalScrollPosition();
-  const scrollPositionRate = (startPosition: number, multiplier: number) =>
-    scrollPosition < startPosition ? 0 : (scrollPosition / startPosition - 1) * multiplier;
-  const headerHeightRate = (scrollPositionRate(900, 2) * 100) / 9;
+  const clientWidth = useWindowWidth();
+  const dynamicStartPoint = clientWidth > 900 ? 900 : clientWidth;
+  const scrollPositionRate = (startPosition: number, multiplier: number) => {
+    return scrollPosition < startPosition ? 0 : (scrollPosition / startPosition - 1) * multiplier;
+  };
+  const headerHeightRate = (scrollPositionRate(dynamicStartPoint, 2) * 100) / 9;
 
   return (
     <>
@@ -14,7 +17,7 @@ export const Header = () => {
         <div className='my-5 flex flex-row items-center justify-center'>
           <div
             className={'text-sm font-semibold uppercase tracking-widest text-gray-500'}
-            style={{ opacity: scrollPositionRate(900, 10) }}>
+            style={{ opacity: scrollPositionRate(dynamicStartPoint, 10) }}>
             Simplify your works
           </div>
         </div>
@@ -24,18 +27,18 @@ export const Header = () => {
           <div className={classNames(STYLE_BLUR_GRADIENT_B_MD, 'h-full w-3')} />
           <div className='h-full w-1 rounded-full bg-gradient-to-b from-blue-600' />
         </div>
-        <div className='text-center'>
+        <div className='px-5 text-center'>
           <h1
-            className='my-5 h-full bg-slate-50 text-4xl font-bold tracking-normal text-slate-800 sm:text-5xl'
+            className='my-5 h-full bg-slate-50 text-3xl font-bold tracking-normal text-slate-800 sm:text-5xl'
             style={{
-              opacity: scrollPositionRate(1280, 10),
+              opacity: scrollPositionRate(dynamicStartPoint * 1.2, 10),
             }}>
             Manage less work better
           </h1>
           <h2
-            className='max-w-2xl text-xl text-slate-600'
+            className='max-w-2xl text-lg text-slate-600 sm:text-xl'
             style={{
-              opacity: scrollPositionRate(1350, 10),
+              opacity: scrollPositionRate(dynamicStartPoint * 1.2, 10),
             }}>
             Unburden yourself from managing time-consuming tasks by allowing app to seamlessly
             choose the most suitable to-dos for you.
