fc628fb14dfcbe558a2e53f9b56e4469bf58b670
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-14 21:57:50 -0500

    feat: optimize Sidebar Open to Prevent Resetting Below 768px
    Optimize the sidebar opening by preventing the resetting of the sidebar
    when the breakpoint is lower than the media queries of 768px. This can
    prevent any unwanted resetting of the sidebar open in a larger screen
    size, resulting in a better user experience.

--- components/layouts/layoutHeader/logo.tsx
+++ components/layouts/layoutHeader/logo.tsx
@@ -1,11 +1,18 @@
 import { PrefetchRouterButton } from '@buttons/button/prefetchRouterButton';
 import { SvgLogo } from '@components/icons/svgLogo';
 import { NetworkStatus } from '@components/notifications/networkStatus';
+import { BREAKPOINT } from '@constAssertions/ui';
 import { useNavigationOpen } from '@hooks/layouts';
+import { atomEffectMediaQuery } from '@states/atomEffects/misc';
 import { Fragment as LogoContainerFragment, Fragment as LogoFragment } from 'react';
+import { useRecoilValueLoadable } from 'recoil';
 
 export const Logo = () => {
+  const breakpointMD = useRecoilValueLoadable(atomEffectMediaQuery(BREAKPOINT['md'])).valueMaybe();
   const setSidebarOpen = useNavigationOpen();
+  const onClickRouterHandler = () => {
+    !breakpointMD && setSidebarOpen();
+  };
   const optionsRouter = { path: '/', className: 'cursor-pointer' };
 
   return (
@@ -13,7 +20,7 @@ export const Logo = () => {
       <LogoContainerFragment>
         <PrefetchRouterButton
           options={optionsRouter}
-          onClick={() => setSidebarOpen()}>
+          onClick={() => onClickRouterHandler()}>
           <LogoFragment>
             <span className='flex w-full flex-row items-center justify-center'>
               <SvgLogo type='MainWhite' />
