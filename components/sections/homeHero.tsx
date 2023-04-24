d244f997c266760e19991cd18a47c6e1c4c4284a
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-24 08:29:54 -0500

    feat: update on sections with additional SmoothTransition
    Add the wrapper to the page index and add a section element to the section
    components. Adjust the transition type on the 'Get started' button in homeHero
    for better consistency. Also, add SmoothTransition components to the
    homeStartToday section component to enhance user experience. The transition
    rate for SmoothTransition is set to 0.7 for a slower transition trigger.

--- components/sections/homeHero.tsx
+++ components/sections/homeHero.tsx
@@ -32,75 +32,77 @@ export const HomeHero = () => {
 
   return (
     <>
-      <div className='relative isolate pt-10'>
-        <div className='py-24 sm:py-32 lg:pb-40'>
-          <div
-            className='mx-auto max-w-7xl px-6 lg:px-8'
-            ref={divRef}
-          >
-            <SmoothTransition options={translateOptions}>
-              <div className='mx-auto max-w-2xl text-center'>
-                <h1 className='mb-2 text-4xl font-bold text-slate-800 will-change-transform sm:text-6xl'>
-                  Simplify your life
-                </h1>
-                <h1 className='text-4xl font-bold text-slate-800 will-change-transform sm:text-6xl'>
-                  Automate your tasks
-                </h1>
-              </div>
-            </SmoothTransition>
-            <SmoothTransition options={{ ...translateOptions, delay: DELAY['300'] }}>
-              <div className='mx-auto max-w-2xl text-center'>
-                <p className='mt-6 text-xl leading-8 text-gray-600 will-change-transform'>
-                  Focus on your work more and manage your to-dos less. Enhance your efficiency and improve
-                  your productivity.
-                </p>
-              </div>
-            </SmoothTransition>
-            <SmoothTransition options={{ ...scaleCenterOptions, delay: DELAY['500'] }}>
-              <div className='mt-10 flex items-center justify-center gap-x-6'>
-                <SignInButton options={signInButtonOptions} />
-                <Link
-                  className='text-sm font-semibold leading-6 text-gray-900'
-                  href={PATH_HOME['features']}
-                >
-                  Learn more <span aria-hidden='true'>→</span>
-                </Link>
-              </div>
-            </SmoothTransition>
-            <div className='flex justify-center'>
-              <div className='relative mt-16 flow-root max-w-[60rem] sm:mt-24'>
-                <SmoothTransition
-                  options={fadeInOptions}
-                  scrollRef={divRef}
-                >
-                  <div
-                    className={classNames(
-                      'absolute h-full w-full rounded-xl will-change-transform',
-                      STYLE_BLUR_GRADIENT_R_LG,
-                    )}
-                  />
-                </SmoothTransition>
-                <SmoothTransition
-                  options={scaleCenterOptions}
-                  scrollRef={divRef}
-                >
-                  <div className='mx-auto flex w-full max-w-[60rem] flex-row items-center justify-center rounded-xl border-none ring-0 lg:rounded-2xl'>
-                    <Image
-                      loader={nextImageLoader}
-                      width={0}
-                      height={0}
-                      className='h-auto w-auto rounded-2xl ring-2 ring-slate-300/20 drop-shadow-2xl will-change-transform'
-                      src={PATH_IMAGE_HOME['demo']}
-                      alt='demo application image'
-                      priority
+      <section className='w-screen'>
+        <div className='relative isolate pt-10'>
+          <div className='py-24 sm:py-32 lg:pb-40'>
+            <div
+              className='mx-auto max-w-7xl px-6 lg:px-8'
+              ref={divRef}
+            >
+              <SmoothTransition options={translateOptions}>
+                <div className='mx-auto max-w-2xl text-center'>
+                  <h1 className='mb-2 text-4xl font-bold text-slate-800 will-change-transform sm:text-6xl'>
+                    Simplify your life
+                  </h1>
+                  <h1 className='text-4xl font-bold text-slate-800 will-change-transform sm:text-6xl'>
+                    Automate your tasks
+                  </h1>
+                </div>
+              </SmoothTransition>
+              <SmoothTransition options={{ ...translateOptions, delay: DELAY['300'] }}>
+                <div className='mx-auto max-w-2xl text-center'>
+                  <p className='mt-6 text-xl leading-8 text-gray-600 will-change-transform'>
+                    Focus on your work more and manage your to-dos less. Enhance your efficiency and improve
+                    your productivity.
+                  </p>
+                </div>
+              </SmoothTransition>
+              <SmoothTransition options={{ ...translateOptions, delay: DELAY['700'] }}>
+                <div className='mt-10 flex items-center justify-center gap-x-6'>
+                  <SignInButton options={signInButtonOptions} />
+                  <Link
+                    className='text-sm font-semibold leading-6 text-gray-900'
+                    href={PATH_HOME['features']}
+                  >
+                    Learn more <span aria-hidden='true'>→</span>
+                  </Link>
+                </div>
+              </SmoothTransition>
+              <div className='flex justify-center'>
+                <div className='relative mt-16 flow-root max-w-[60rem] sm:mt-24'>
+                  <SmoothTransition
+                    options={fadeInOptions}
+                    scrollRef={divRef}
+                  >
+                    <div
+                      className={classNames(
+                        'absolute h-full w-full rounded-xl will-change-transform',
+                        STYLE_BLUR_GRADIENT_R_LG,
+                      )}
                     />
-                  </div>
-                </SmoothTransition>
+                  </SmoothTransition>
+                  <SmoothTransition
+                    options={scaleCenterOptions}
+                    scrollRef={divRef}
+                  >
+                    <div className='mx-auto flex w-full max-w-[60rem] flex-row items-center justify-center rounded-xl border-none ring-0 lg:rounded-2xl'>
+                      <Image
+                        loader={nextImageLoader}
+                        width={0}
+                        height={0}
+                        className='h-auto w-auto rounded-2xl ring-2 ring-slate-300/20 drop-shadow-2xl will-change-transform'
+                        src={PATH_IMAGE_HOME['demo']}
+                        alt='demo application image'
+                        priority
+                      />
+                    </div>
+                  </SmoothTransition>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
