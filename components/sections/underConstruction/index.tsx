6cf227bfedabc926a543adee7f9ff2d402356524
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-25 09:56:12 -0500

    feat: enhance grid rendering with max-width constraint
    Enhance grid rendering by implementing a max-width constraint, which
    effectively limits the grid's width expansion, preventing it from increasing
    indefinitely.

--- components/sections/underConstruction/index.tsx
+++ components/sections/underConstruction/index.tsx
@@ -8,8 +8,8 @@ export const UnderConstruction = () => {
   const styleParagraph = 'text-3xl font-bold uppercase tracking-wide ml:text-4xl';
 
   return (
-    <>
-      <div className='grid-row-2 grid items-center justify-items-center gap-6 px-2 py-5 sm:px-10 md:px-20 ml:grid-cols-2'>
+    <section className='flex flex-row items-center justify-center'>
+      <div className='grid-row-2 grid w-full max-w-7xl items-center justify-items-center gap-6 px-2 py-5 sm:px-10 md:px-20 ml:grid-cols-2'>
         <div className='flex max-w-md flex-col items-center justify-center space-y-6 p-2 text-center'>
           <div className='flex flex-col items-center justify-center space-y-2 text-slate-800/90'>
             <h1 className={classNames(styleParagraph)}>Under</h1>
@@ -17,9 +17,8 @@ export const UnderConstruction = () => {
           </div>
           <div>
             <p className='text-slate-800/80'>
-              The page is currently under construction for improvements to better serve you. We
-              appreciate your patience. Please check back soon for an upgraded and user-friendly
-              experience. Thank you!
+              The page is currently under construction for improvements to better serve you. We appreciate
+              your patience. Please check back soon for an upgraded and user-friendly experience. Thank you!
             </p>
           </div>
           <Link
@@ -27,7 +26,8 @@ export const UnderConstruction = () => {
             className={classNames(
               STYLE_BUTTON_FULL_BLUE,
               'flex w-full max-w-xs flex-row justify-center whitespace-nowrap',
-            )}>
+            )}
+          >
             Back to homepage
           </Link>
         </div>
@@ -43,6 +43,6 @@ export const UnderConstruction = () => {
           />
         </div>
       </div>
-    </>
+    </section>
   );
 };
