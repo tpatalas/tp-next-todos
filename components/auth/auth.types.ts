a47f414ff8afa18818483c379dd67f1113edceb3
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-05-01 17:01:45 -0500

    test: enhance AuthErrorMessage component test coverage
    Expand testing coverage for the AuthErrorMessage component, which is
    responsible for displaying both server-side and client-side error messages.
    The added tests primarily focus on verifying the accurate rendering of
    error messages based on different query parameters and error conditions.

--- components/auth/auth.types.ts
+++ components/auth/auth.types.ts
@@ -2,3 +2,14 @@ export interface TypesNextAuthError {
   _id: string;
   message: string;
 }
+
+export interface TypesAuthErrorMessage {
+  isError: boolean;
+  isSignIn: boolean;
+  defaultMessage: string;
+  errorMessage: string;
+}
+
+export type TypesPropsErrorMessage = {
+  options?: Partial<TypesAuthErrorMessage>;
+};
