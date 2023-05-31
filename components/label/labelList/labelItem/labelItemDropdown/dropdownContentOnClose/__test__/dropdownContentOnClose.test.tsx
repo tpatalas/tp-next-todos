3d589b8e5a414615db20e457395da58d56718b02
tpatalas <tp@atalas.dev>
2023-05-31 13:11:39 -0500

    test: Enhance Testing Suite with DropdownContentOnClose
    A new integration test for the DropdownContentOnClose component has been added.
    The unique requirements of this component, given its derived state from the
    Recoil state management, necessitates a controlled testing environment.
    Through the application of `UserSessionEffect`, `mockedLabelItem`, and
    `demoLabelItem`, a structured environment for comprehensive component
    testing is effectively established.

---
+++ components/label/labelList/labelItem/labelItemDropdown/dropdownContentOnClose/__test__/dropdownContentOnClose.test.tsx
@@ -0,0 +1,63 @@
+import { Labels } from '@label/label.types';
+import { renderWithRecoilRootAndSession } from '@stateLogics/utils/testUtils';
+import { screen, waitFor } from '@testing-library/react';
+import { DropdownContentOnClose } from '..';
+import { mockedLabelItem } from '__mock__/label';
+import { DATA_DEMO_LABELS } from '@label/label.data';
+import { useSetRecoilState } from 'recoil';
+import { atomUserSession } from '@user/user.states';
+import { useEffect } from 'react';
+
+const UserSessionEffect = () => {
+  const setUserSession = useSetRecoilState(atomUserSession);
+
+  useEffect(() => {
+    setUserSession(false);
+  }, [setUserSession]);
+  return null;
+};
+
+describe('DropdownContentOnClose', () => {
+  const renderWithDropdownContentOnClose = (mockedLabelItem: Labels) => {
+    const options = { session: null };
+    return renderWithRecoilRootAndSession(
+      <>
+        <DropdownContentOnClose label={mockedLabelItem} />
+        <UserSessionEffect />
+      </>,
+      options,
+    );
+  };
+
+  it('should not render the todoCount number when mockedLabelItem is used', async () => {
+    const { container } = renderWithDropdownContentOnClose(mockedLabelItem);
+
+    expect(container).toBeInTheDocument();
+
+    await waitFor(() => {
+      const todoCount = screen.queryByText('1');
+      expect(todoCount).not.toBeInTheDocument();
+    });
+  });
+
+  it('should render the todoCount number when demoLabelItem is used', async () => {
+    const demoLabelId = '201';
+    const demoLabelItem = DATA_DEMO_LABELS.find((label) => label._id === demoLabelId) || ({} as Labels);
+    const { container } = renderWithDropdownContentOnClose(demoLabelItem);
+    const demoTodoCount = demoLabelItem.title_id?.length as number;
+    const demoTodoCountString = demoTodoCount.toString() || '';
+    const demoTodoCountControl = demoTodoCount + 1;
+
+    expect(container).toBeInTheDocument();
+
+    await waitFor(() => {
+      const todoCount = screen.queryByText(demoTodoCountString);
+      expect(todoCount).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      const todoCount = screen.queryByText(demoTodoCountControl);
+      expect(todoCount).not.toBeInTheDocument();
+    });
+  });
+});
