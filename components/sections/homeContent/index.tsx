dec19a2209f3fe8f7923ab01bc251f2f10044344
tpatalas <tp@atalas.dev>
2023-06-04 16:11:37 -0500

    fix: Address Cumulative Layout Shift issues
    Improve the Cumulative Layout Shift (CLS) by incorporating the
    SmoothTransition components. In the absence of a `useRef` reference,
    SmoothTransition component allows for immediate rendering,
    eliminating delays in animation and subsequently enhancing the
    CLS score.

--- components/sections/homeContent/index.tsx
+++ components/sections/homeContent/index.tsx
@@ -15,6 +15,7 @@ export const HomeContent = () => {
   const imageOptions = {
     type: TRANSITION_TYPE['scaleCenterSm'],
     enterDuration: DURATION['700'],
+    rate: 1.5,
   };
   const spotlightRef = useRef(null);
   const overloadRef = useRef(null);
@@ -22,67 +23,69 @@ export const HomeContent = () => {
   const overloadImageRef = useRef(null);
 
   return (
-    <div className='w-screen'>
-      <div className='relative my-24 flex flex-row items-center justify-center px-5 py-10 sm:px-10 md:my-32 lg:px-28'>
-        <div className='md:min-w-3/4 grid w-full max-w-6xl grid-cols-1 items-center justify-items-center gap-10 md:grid-cols-2'>
-          <ContentText
-            title='Spotlight your to-dos'
-            subTitle="View your to-dos that are intelligently and automatically selected in Today's Focus."
-            content="Add your to-dos as you please, with or without due dates and priorities. Today's Focus will display your most important to-dos for you."
-            scrollRef={spotlightRef}
-          />
-          <div
-            ref={spotlightImageRef}
-            className={classNames(styleImageFrame)}
-          >
-            <SmoothTransition
-              options={{ ...imageOptions, delay: DELAY['500'] }}
-              scrollRef={spotlightImageRef}
+    <SmoothTransition>
+      <div className='w-screen'>
+        <div className='relative my-24 flex flex-row items-center justify-center px-5 py-10 sm:px-10 md:my-32 lg:px-28'>
+          <div className='md:min-w-3/4 grid w-full max-w-6xl grid-cols-1 items-center justify-items-center gap-10 md:grid-cols-2'>
+            <ContentText
+              title='Spotlight your to-dos'
+              subTitle="View your to-dos that are intelligently and automatically selected in Today's Focus."
+              content="Add your to-dos as you please, with or without due dates and priorities. Today's Focus will display your most important to-dos for you."
+              scrollRef={spotlightRef}
+            />
+            <div
+              ref={spotlightImageRef}
+              className={classNames(styleImageFrame)}
             >
-              <div className={classNames(styleImageWrapper, 'opacity-100')}>
-                <Image
-                  loader={cloudflareLoader}
-                  width={3000}
-                  height={1000}
-                  className={styleImage}
-                  src={PATH_IMAGE_HOME['contentFocus']}
-                  alt='content focus image'
-                  priority={true}
-                />
-              </div>
-            </SmoothTransition>
-          </div>
-          <div
-            className={classNames('max-md:order-last', styleImageFrame)}
-            ref={overloadImageRef}
-          >
-            <SmoothTransition
-              options={{ ...imageOptions, delay: DELAY['1000'] }}
-              scrollRef={overloadImageRef}
+              <SmoothTransition
+                options={{ ...imageOptions, delay: DELAY['500'] }}
+                scrollRef={spotlightImageRef}
+              >
+                <div className={classNames(styleImageWrapper, 'opacity-100')}>
+                  <Image
+                    loader={cloudflareLoader}
+                    width={3000}
+                    height={1000}
+                    className={styleImage}
+                    src={PATH_IMAGE_HOME['contentFocus']}
+                    alt='content focus image'
+                    priority={true}
+                  />
+                </div>
+              </SmoothTransition>
+            </div>
+            <div
+              className={classNames('max-md:order-last', styleImageFrame)}
+              ref={overloadImageRef}
             >
-              <div className={classNames(styleImageWrapper, 'opacity-100')}>
-                <Image
-                  loader={cloudflareLoader}
-                  width={3000}
-                  height={1000}
-                  className={styleImage}
-                  src={PATH_IMAGE_HOME['contentOrganize']}
-                  alt='content organize image'
-                  priority={true}
-                />
-              </div>
-            </SmoothTransition>
-          </div>
-          <ContentText
-            title='Free your overload'
-            subTitle='Work on a to-do list that is auto-allocated according to your capacity.'
-            content="Today's Focus efficiently determines the ideal number of to-dos for you. As you
+              <SmoothTransition
+                options={{ ...imageOptions, delay: DELAY['1000'] }}
+                scrollRef={overloadImageRef}
+              >
+                <div className={classNames(styleImageWrapper, 'opacity-100')}>
+                  <Image
+                    loader={cloudflareLoader}
+                    width={3000}
+                    height={1000}
+                    className={styleImage}
+                    src={PATH_IMAGE_HOME['contentOrganize']}
+                    alt='content organize image'
+                    priority={true}
+                  />
+                </div>
+              </SmoothTransition>
+            </div>
+            <ContentText
+              title='Free your overload'
+              subTitle='Work on a to-do list that is auto-allocated according to your capacity.'
+              content="Today's Focus efficiently determines the ideal number of to-dos for you. As you
             consistently complete to-dos, the process adjusts and assigns more or fewer to-dos base
             on your completion rate."
-            scrollRef={overloadRef}
-          />
+              scrollRef={overloadRef}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </SmoothTransition>
   );
 };
