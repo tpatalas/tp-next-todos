3015caddf64fbe339a73b169547a5c128437c3f0
tpatalas <tp@atalas.dev>
2023-07-18 12:36:13 -0500

    refactor: Supplement tests for Tooltip visibility
    Supplement integration and unit testing for Tooltip text visibility. Additional
    tests focus on ButtonWithTooltip and Tooltip components, verifying proper
    rendering when visibility is enabled.

--- app/components/ui/button/buttonWithTooltip/__test__/buttonWithTooltip.test.tsx
+++ app/components/ui/button/buttonWithTooltip/__test__/buttonWithTooltip.test.tsx
@@ -1,13 +1,21 @@
 import { fireEvent, render } from '@testing-library/react';
 import { ButtonWithTooltip } from '..';
 import { screen } from '@testing-library/react';
+import { PropsButtonWithTooltip } from '@/button/button.types';
 
 const mockOnClick = jest.fn();
 const mockButtonElement = 'Test button element';
 
 describe('ButtonWithTooltip', () => {
-  const renderWithButtonWithTooltip = () =>
-    render(<ButtonWithTooltip onClick={mockOnClick}>{mockButtonElement}</ButtonWithTooltip>);
+  const renderWithButtonWithTooltip = ({ options }: PropsButtonWithTooltip = {}) =>
+    render(
+      <ButtonWithTooltip
+        options={options}
+        onClick={mockOnClick}
+      >
+        {mockButtonElement}
+      </ButtonWithTooltip>,
+    );
 
   it('should render button element and tooltip element', () => {
     const { container } = renderWithButtonWithTooltip();
@@ -25,5 +33,12 @@ describe('ButtonWithTooltip', () => {
     fireEvent.click(buttonElement);
 
     expect(mockOnClick).toHaveBeenCalled();
+  });
+
+  it('should render the tooltip text', () => {
+    renderWithButtonWithTooltip({ options: { isVisible: true, tooltip: 'button-tooltip-test' } });
+    const tooltipText = screen.getByText('button-tooltip-test');
+
+    expect(tooltipText).toBeInTheDocument();
   });
 });
