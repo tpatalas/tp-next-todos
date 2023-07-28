a8768c7275593ac3d2c9f9b5970384f66a419c94
tpatalas <tp@atalas.dev>
2023-07-28 13:32:39 -0500

    feat: Extend appRouter with SectionContent component
    Incorporates the SectionContent component into appRouter, as a refactored
    variant of HomeContent from pageRouter. Ensures the validity of the change
    through integration tests, assessing the rendering of text content and
    mocked image component.
    
    Enhances SectionHeader and SectionHero components with the inclusion of
    previously omitted className attributes.

---
+++ app/components/section/sectionContent/__test__/sectionContent.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { SectionContent } from '..';
+import { sectionContentTextContents } from '@/section/section.consts';
+import { ReactNode } from 'react';
+
+jest.mock('@/components/next/imageWithRemotePlaceholder', () => ({
+  ImageWithRemotePlaceholder: () => <div data-testid='mockImage-testid' />,
+}));
+
+jest.mock('@/transition/smoothTransitionWithDivRef', () => ({
+  SmoothTransitionWithDivRef: ({ children }: { children: ReactNode }) => <div>{children}</div>,
+}));
+
+describe('SectionContent', () => {
+  const renderWithSectionContent = () => render(<SectionContent />);
+
+  it('should render the content texts of spotlight and overload', () => {
+    const { container } = renderWithSectionContent();
+
+    Object.values(sectionContentTextContents).forEach((contentObjects) => {
+      Object.values(contentObjects).forEach(async (value) => {
+        await waitFor(() => {
+          const text = screen.queryByText(value);
+          expect(text).toBeInTheDocument();
+        });
+      });
+    });
+    expect(container).toBeInTheDocument();
+  });
+
+  it('should mount the ImageWithRemotePlaceholder properly', async () => {
+    renderWithSectionContent();
+    await waitFor(() => {
+      const imageTestId = screen.getAllByTestId('mockImage-testid');
+
+      imageTestId.forEach((value) => {
+        expect(value).toBeInTheDocument();
+      });
+    });
+  });
+});
