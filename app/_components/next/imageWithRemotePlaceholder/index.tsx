0fdd45f0f24bae64f0c697de83317aa72fc6444d
tpatalas <tp@atalas.dev>
2023-08-21 13:14:12 -0500

    feat: test

--- app/_components/next/imageWithRemotePlaceholder/index.tsx
+++ app/_components/next/imageWithRemotePlaceholder/index.tsx
@@ -1,16 +1,11 @@
 import 'server-only';
-import getBase64FromImageURL from '@/_lib/utils/base64Converter.utils';
 import Image from 'next/image';
 import { PropsImageWithRemotePlaceholder } from './imageWithRemotePlaceholder.types';
 import cloudflareLoader from '@/_lib/utils/imageLoader.utils';
 
 export const ImageWithRemotePlaceholder = async ({ configs }: PropsImageWithRemotePlaceholder) => {
   const { sizes = '100vw', priority = true } = configs;
-  const remoteImageHandler = async () => {
-    if (configs.placeholder === 'blur') return await getBase64FromImageURL(configs.src);
-    return;
-  };
-  const remoteImageBlurDataURL = await remoteImageHandler();
+  // const remoteImageBlurDataURL = await getBase64FromImageURL(configs.src);
 
   return (
     <>
@@ -23,10 +18,10 @@ export const ImageWithRemotePlaceholder = async ({ configs }: PropsImageWithRemo
         src={configs.src}
         fill={configs.fill}
         alt={configs.alt}
-        placeholder={configs.placeholder}
+        // placeholder={configs.placeholder}
         style={configs.style}
         loading={configs.loading}
-        blurDataURL={remoteImageBlurDataURL}
+        // blurDataURL={configs.placeholder && remoteImageBlurDataURL}
         sizes={sizes}
         priority={priority}
       />
