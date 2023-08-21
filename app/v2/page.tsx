0d73b0c8656df005e8fb9007e22e372cae351c0c
tpatalas <tp@atalas.dev>
2023-08-21 15:21:03 -0500

    fest: test

--- app/v2/page.tsx
+++ app/v2/page.tsx
@@ -1,15 +1,10 @@
-import { SectionContent } from '@/section/sectionContent';
-import { SectionHeader } from '@/section/sectionHeader';
-import { SectionHero } from '@/section/sectionHero';
-import { SectionStartToday } from '@/section/sectionStartToday';
-
 const Home = () => {
   return (
     <>
-      <SectionHero />
-      <SectionHeader />
-      <SectionContent />
-      <SectionStartToday />
+      {/* <SectionHero /> */}
+      {/* <SectionHeader /> */}
+      {/* <SectionContent /> */}
+      {/* <SectionStartToday /> */}
     </>
   );
 };
