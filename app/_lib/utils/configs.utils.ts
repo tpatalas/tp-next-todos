31447a01f34acaec7798ce326c2cd26357276bd5
tpatalas <tp@atalas.dev>
2023-08-26 21:17:01 -0500

    feat: Enhance createConfigs for null and undefined
    Allow null and undefined within presetOptions and defaultOptions.
    Null values now explicitly map to null in output. Undefined values
    maintain previous behavior of omitting the property. Aimed at providing
    clearer function usage guidelines.

--- app/_lib/utils/configs.utils.ts
+++ app/_lib/utils/configs.utils.ts
@@ -8,7 +8,7 @@ type TypesOptions<T, S extends string> = {
 type TypesConfigs<T, P extends string> = {
   options: T;
   defaultOptions: Partial<{ [K in keyof T]: keyof T[K] | null | undefined }>;
-  presetOptions?: Partial<Record<P, Partial<{ [K in keyof T]: keyof T[K] }>>>;
+  presetOptions?: Partial<Record<P, Partial<{ [K in keyof T]: keyof T[K] | null | undefined }>>>;
 };
 
 export const createConfigs = <T extends TypesOptions<T, S>, S extends string, P extends string = never>(
@@ -19,17 +19,23 @@ export const createConfigs = <T extends TypesOptions<T, S>, S extends string, P 
   return (props?: Partial<{ [K in keyof T]: keyof T[K] } & { preset?: P }>): { [K in keyof T]: T[K][keyof T[K]] } => {
     const { defaultOptions, options, presetOptions } = config;
 
-    const result: { [K in keyof T]?: T[K][keyof T[K]] } = {};
+    const result: { [K in keyof T]?: T[K][keyof T[K]] | null } = {};
     const preset = props?.preset ? presetOptions?.[props.preset] : undefined;
 
     for (const key in options) {
       const k = key as keyof T;
-      const defaultOptionsOutput = defaultOptions ? defaultOptions[k] : undefined;
-      const presetOptionsOutput = preset ? preset[k] : undefined;
-      const variant = props?.[k] ?? presetOptionsOutput ?? defaultOptionsOutput;
+      const getVariant = (k: keyof T) => {
+        if (props?.hasOwnProperty(k)) return props[k];
+        if (preset?.hasOwnProperty(k)) return preset[k] !== undefined ? preset[k] : defaultOptions[k];
+        return defaultOptions[k];
+      };
+      const variant = getVariant(k);
 
-      if (!!variant && options[k].hasOwnProperty(variant)) {
+      if (variant !== undefined && options[k].hasOwnProperty(variant as PropertyKey)) {
         result[k] = options[k][variant as keyof T[keyof T]];
+      }
+      if (variant === null) {
+        result[k] = null;
       }
     }
 
