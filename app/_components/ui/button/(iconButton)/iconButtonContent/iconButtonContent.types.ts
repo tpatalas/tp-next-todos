62b418b62b958bcb231a6881dc2e42f82ede4c48
tpatalas <tp@atalas.dev>
2023-09-14 12:17:27 -0500

    feat: Add the unit test for IconButtonContent
    Add the simple unit test for the new component called IconButtonContent.

--- app/_components/ui/button/(iconButton)/iconButtonContent/iconButtonContent.types.ts
+++ app/_components/ui/button/(iconButton)/iconButtonContent/iconButtonContent.types.ts
@@ -2,6 +2,6 @@ import { TypesButton } from '@/button/button.types';
 import { PropsSvgIcon } from '@/icon/icon.types';
 import { ReactNode } from 'react';
 
-export type PropsIconButtonContent = PropsSvgIcon &
-  Partial<{ children: ReactNode; extraContent: ReactNode }> &
-  TypesButton;
+export type PropsIconButtonContent = Partial<
+  { children: ReactNode; extraContent: ReactNode } & TypesButton & PropsSvgIcon
+>;
