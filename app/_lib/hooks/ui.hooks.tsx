b9b0d9f1e8b4c17fc5cb12b5ee08945167a8feda
tpatalas <tp@atalas.dev>
2023-09-15 19:57:37 -0500

    feat: Add the appRouter custom hook
    Add ui related custom hook that can detect the vertical scroll position.

---
+++ app/_lib/hooks/ui.hooks.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+export const useVerticalScrollPosition = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return scrollPosition;
+};
