bd8815741fedfc65769d67c8f32b3894049dfbc4
tpatalas <tp@atalas.dev>
2023-09-19 10:33:38 -0500

    feat: Migrate DividerX and DividerY to appRouter
    Move DividerX and DividerY components from pageRouter to appRouter.
    Apply minor modifications and include unit tests for each component.

---
+++ app/_components/ui/divider/divider.types.ts
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+export interface PropsDividerX {
+  children?: ReactNode;
+  style: HTMLElement['className'];
+}
+
+export interface PropsDividerY {
+  style: HTMLElement['className'];
+}
